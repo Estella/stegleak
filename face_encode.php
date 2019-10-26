3b31831c25580f361fda3731c1699c073915ac1e
Estella Mystagic <Selphie.Keller@gmail.com>
2019-10-26 12:28:36 -0600

    Update face_encode.php

--- face_encode.php
+++ face_encode.php
@@ -1,8 +1,7 @@
 <?php
-// error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
-set_time_limit(0);
 include "class_facesteg.php";
 
+// Prevent Caching
 header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
