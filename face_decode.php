4dff4ed85a1facdca102b94ed1f1623d1ba0811e
Estella Mystagic <Selphie.Keller@gmail.com>
2019-10-26 12:28:05 -0600

    Update face_decode.php

--- face_decode.php
+++ face_decode.php
@@ -1,13 +1,13 @@
 <?php
-// error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
-set_time_limit(0);
 include "class_facesteg.php";
 
+// Prevent Caching
 header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+
 header('Content-type: text/plain');
 $detector = new Face_Steg('detection.dat',5);
 $detector->face_detect('Encoded_01.png');
